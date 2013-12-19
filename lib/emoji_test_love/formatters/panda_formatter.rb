e5dd2847a311dc9be4986feb04f878196b2ed7ab
Peter Brändel <peter@bitbites.de>
2013-12-19 10:55:44 +0100

    Finished the panda. :alien:

--- lib/emoji_test_love/formatters/panda_formatter.rb
+++ lib/emoji_test_love/formatters/panda_formatter.rb
@@ -2,15 +2,15 @@
 module EmojiTestLove
   class Panda
     def passed_display
-      "\u{1f43c}"
+      "\u{1f43c} "
     end
 
     def failed_display
-      "\u{1f43c}"
+      "\u{1f4a9} "
     end
 
     def pending_display
-      "\u{1f43c}"
+      "\u{1f4a4} "
     end
     
     def names
