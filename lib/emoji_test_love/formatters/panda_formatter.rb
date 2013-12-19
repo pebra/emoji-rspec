a913154516e5406f0b74e0b091e414d318f0ed31
Peter Brändel <peter@bitbites.de>
2013-12-19 10:31:15 +0100

    Added some Pandas

---
+++ lib/emoji_test_love/formatters/panda_formatter.rb
@@ -0,0 +1,19 @@
+module EmojiTestLove
+  class Panda
+    def passed_display
+      "\u{1f43c}"
+    end
+
+    def failed_display
+      "\u{1f43c}"
+    end
+
+    def pending_display
+      "\u{1f43c}"
+    end
+    
+    def names
+      'panda'
+    end
+  end
+end
